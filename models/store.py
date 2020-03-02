7402ba68ab70f033ece505b4f69b6fb65f1ddc30
huan <327485253@163.com>
2020-03-03 00:02:22 +0800

    Before push to remote

--- models/store.py
+++ models/store.py
@@ -5,6 +5,7 @@ class StoreModel(db.Model):
 
     __tablename__='stores'
     id = db.Column(db.Integer, primary_key = True)
+
     name = db.Column(db.String(80))
     price = db.Column(db.Float(precision = 2))
 
